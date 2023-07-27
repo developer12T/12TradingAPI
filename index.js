7276f4aeed590f8e786e976373ca7b27704fbb74
aphinet12T <aphinet.dam@onetwotrading.co.th>
2023-07-27 09:52:42 +0700

    Index : Commit
    Removed res.header

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ const server = http.createServer(app)
 const { API_PORT } = process.env
 const port = process.env.port || API_PORT
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, application/json');
-//     next();
-//   });
- 
-
 server.listen(port,()=>{
     console.log('server is running')
 })
