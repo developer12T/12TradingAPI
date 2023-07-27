88958f4e47285d22d48b53b2c879bcda21c3d9ce
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-07-27 15:18:58 +0700

    Update getStock.js

--- M3API/controller/getStock.js
+++ M3API/controller/getStock.js
@@ -2,21 +2,23 @@ const express = require("express");
 const getStock = express.Router();
 const { Stock } = require("../model/stock");
 const { sequelize } = require("../config/dbconnect");
+const {Sequelize} = require('sequelize')
 const { Op } = require("sequelize");
 
 getStock.post("/getStock", async (req, res) => {
   try {
 
-  
     const data = await Stock.findAll({
-      attributes: { exclude: ["id"] },
+      attributes: { 
+        exclude: ["id"],
+        include: [ 
+          [Sequelize.literal('MLSTQT - MLALQT'), 'available']
+        ]
+      }
     });
-    for (const gdata of data) {
-      gdata.sumCol = gdata.balance + gdata.allocated;
-      console.log(gdata)
-    }
+
     res.json(data);
-    
+
   } catch (error) {
     console.error(error);
     res.json(error);
