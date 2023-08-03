ed3506df6cd098131e3e2a82847538a5785a6c7b
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-08-03 13:23:42 +0700

    clear

--- M3API/route/stock.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const StockManage = express.Router();
-
-const Stock = require('../controller/getStock')
-
-StockManage.use('/stock',Stock);
-
-module.exports = StockManage;
